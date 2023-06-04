9106be133b200233057673a59c41a9117422d650
k-mabbott <kmabbott9@gmail.com>
2023-06-04 14:46:28 -0700

    commit

---
+++ Intro/sort.js
@@ -0,0 +1,13 @@
+
+
+// FOR ALPHA
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+fruits.reverse();
+
+
+// FOR NUMBERS
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a-b});
+console.log(points)
+

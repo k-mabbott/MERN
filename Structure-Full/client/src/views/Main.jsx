683da6dd72b89d084adecc31aadf8d93c2b28743
k-mabbott <kmabbott9@gmail.com>
2023-06-28 17:11:16 -0700

    commit

---
+++ Structure-Full/client/src/views/Main.jsx
@@ -0,0 +1,24 @@
+
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios'
+
+// import { useSearchParams } from 'react-router-dom';
+
+
+
+export default () => {
+    
+    // const [ searchParams, setSearchParams ] = useSearchParams()
+
+    // console.log(searchParams.get('code'))
+
+    return (
+        <div>
+            <h1>Hello World!!</h1>
+        </div>
+    )
+}
+
+
+
